d6edf69c67bf101854fe5f109023181c1ee8c5c0
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-24 00:03:36 +0200

    kakkaa kakkaa

--- Assets/Scripts/LeftArmRocket.cs
+++ Assets/Scripts/LeftArmRocket.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class LeftArmRocket : MonoBehaviour {
 
 	public float forceMultiplier = 1000f;
+	public ParticleSystem particles;
 
 	void Update() {
 		if (Input.GetButton("d")) {
-			rigidbody2D.AddRelativeForce(Vector2.right * forceMultiplier * Time.deltaTime);
+			rigidbody2D.AddRelativeForce(Vector2.right * -forceMultiplier * Time.deltaTime);
+		}
+		if (Input.GetButtonDown("d")) {
+			particles.Emit(20);
+		}
+		if (Input.GetButtonUp("d")) {
+			particles.Emit(0);
 		}
 	}
 
--- Assets/Scripts/RightArmRocket.cs
+++ Assets/Scripts/RightArmRocket.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class RightArmRocket : MonoBehaviour {
 	
 	public float forceMultiplier = 1000f;
-	
+	public ParticleSystem particles;
+
+	void Start() {
+		particleSystem.emissionRate = 0;
+	}
+
 	void Update() {
 		if (Input.GetButton("k")) {
 			rigidbody2D.AddRelativeForce(Vector2.right * forceMultiplier * Time.deltaTime);
 		}
+		if (Input.GetButtonDown("k")) {
+			particles.Emit(20);
+		}
+		if (Input.GetButtonUp("k")) {
+			particles.Emit(0);
+		}
 	}
-	
+
 }
