267540028ccc972a144c5a6b2f97da8cef8f76cb
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-24 00:29:12 +0200

    kaikk iei ookkaa kakkaa

--- Assets/Scripts/LeftArmRocket.cs
+++ Assets/Scripts/LeftArmRocket.cs
@@ -4,17 +4,10 @@ using System.Collections;
 public class LeftArmRocket : MonoBehaviour {
 
 	public float forceMultiplier = 1000f;
-	public ParticleSystem particles;
 
 	void Update() {
 		if (Input.GetButton("d")) {
-			rigidbody2D.AddRelativeForce(Vector2.right * -forceMultiplier * Time.deltaTime);
-		}
-		if (Input.GetButtonDown("d")) {
-			particles.Emit(20);
-		}
-		if (Input.GetButtonUp("d")) {
-			particles.Emit(0);
+			rigidbody2D.AddRelativeForce(Vector2.right * forceMultiplier * Time.deltaTime);
 		}
 	}
 
