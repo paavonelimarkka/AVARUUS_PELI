166f6b35f82b1cdafa8f0743a2f53f2298cc626d
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-25 00:08:38 +0200

    add logic

---
+++ Assets/Scripts/bilecounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bilecounter : MonoBehaviour {
+
+
+	void Start () {
+		StartCoroutine(BileCounter());
+	}
+
+
+	IEnumerator BileCounter() {
+		yield return new WaitForSeconds(24);
+		Application.LoadLevel("MainScene");
+	}
+	
+}
---
+++ Assets/Scripts/intrologic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class intrologic : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("space")) {
+			Application.LoadLevel("bilekuva");
+		}
+	}
+}
