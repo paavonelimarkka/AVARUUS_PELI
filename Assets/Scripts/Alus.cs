21c75518068d8f06f0b1f761a189106f3d383d40
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-25 00:30:48 +0200

    joko

---
+++ Assets/Scripts/Alus.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alus : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Application.LoadLevel("loppuruutu");
+	}
+
+}
