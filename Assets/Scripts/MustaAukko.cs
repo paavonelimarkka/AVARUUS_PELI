6aad0c43ef7e7ce145655103662a3dd06219f141
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-24 22:07:15 +0200

    add script for musta aukko

---
+++ Assets/Scripts/MustaAukko.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MustaAukko : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D collider) {
+		Application.LoadLevel("gameover");
+	}
+
+}
