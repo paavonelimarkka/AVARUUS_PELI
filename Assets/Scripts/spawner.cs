a16fa7366a945b9291d88a97fd0e82767e74d8ea
paavonelimarkka <paavo.nelimarkka@gmail.com>
2015-01-25 02:58:17 +0200

    add meteorspawner and logic

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+
+	public bool koira = true;
+	public Rigidbody2D Asteroidi;
+	public float meteorSpeed;
+	public float waitTime;
+	private Transform Spawn;
+
+
+	void Start () {
+		StartCoroutine(Spawner());
+	}
+
+	IEnumerator Spawner() {
+		while (koira = true) {
+			yield return new WaitForSeconds(waitTime);
+			Rigidbody2D clone = (Rigidbody2D)Instantiate(Asteroidi,transform.position,transform.rotation);
+			clone.AddRelativeForce(Vector2.up * meteorSpeed);
+		}
+	}
+	
+}
